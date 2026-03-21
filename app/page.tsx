92a897d66f0fa90f300b162f0656552d2c274b4d
Isabella Lamprecht <isabellallamrecth03@gmail.com>
2026-03-21 23:05:55 +0200

    fix syntax

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@
 export default function Home() {
   return (
       <main style={{ padding: "40px" }}>
-        <h1>Ruckings Live<h1>
+        <h1>Ruckings Live</h1>
         <p>App werk nou op Vercel!</p>
       </main>
   );
-}
