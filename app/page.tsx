740dd1224397557efc8f239133a391427534a08a
isabellallamprecth03-lang <isabellallamprecth03@gmail.com>
2026-03-22 08:22:28 +0200

    fix homepage clean

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-      <main style={{ padding: "40px" }}>
-        <h1>Ruckings Live</h1>
-        <p>App werk nou op Vercel!</p>
-      </main>
+    <main>
+      <h1> Ruckings Live</h1>
+      <p> Site werk nou! </p>
+    </main>
   );
 }
